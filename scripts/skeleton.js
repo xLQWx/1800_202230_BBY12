40a3158dbc329a5efff39638fd903fc2bfa006fb
Yvonne Li <qli119@my.bcit.ca>
2022-10-28 16:37:18 -0700

    re-organize the htmls and folder structure, added template.html and verion 2 of index.html

---
+++ scripts/skeleton.js
@@ -0,0 +1,10 @@
+//---------------------------------------------------
+// This function loads the parts of your skeleton 
+// (navbar, footer, and other things) into html doc. 
+//---------------------------------------------------
+function loadSkeleton(){
+    console.log($('#navbarPlaceholder').load('./text/nav.html'));
+    console.log($('#footerPlaceholder').load('./text/footer.html'));
+    console.log($('#featurePlaceholder').load('./text/featureList.html'));
+}
+loadSkeleton();  //invoke the function
